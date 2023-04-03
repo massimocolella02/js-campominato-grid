2b10ec673102dffced8cf69948268946d7f86f9b
massimocolella02 <massimo-colella@outlook.it>
2023-04-03 16:42:42 +0200

    Esercizio Completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
+
+//variabili
 let difficoltà = document.getElementById('difficult').value;
 let box = document.createElement('div');
 let grid = document.querySelector('.grid');
 
 
-
+//Al click compare il gioco in base alle difficoltà
 function play(){
     difficoltà = document.getElementById('difficult').value;
 
@@ -18,24 +20,33 @@ function play(){
             hard();
         default:
     }
+
 }
 
 
 
 /*--------------------------------------------Funzioni------------------------------------------------------*/
 function easy(){
+    //Cancella ogni div se c'è bisogno
     while(grid.firstChild){
         grid.removeChild(grid.firstChild);
     }
 
+    //Crea i box nel grid
     for( let i=1; i <= 100; i++){
         box = document.createElement('div');
         box.classList.add('box');
         box.classList.add('box-easy');
         grid.appendChild(box);
         box.innerHTML= i;
+
+        box.addEventListener('click', function(){
+            this.classList.add('bg-azzurro');
+            console.log(i)
+        })
     }
     return box
+
 }
 
 function medium(){
@@ -49,6 +60,11 @@ function medium(){
         box.classList.add('box-medium');
         grid.appendChild(box);
         box.innerHTML= i
+
+        box.addEventListener('click', function(){
+            this.classList.add('bg-azzurro');
+            console.log(i)
+        })
     }
     return box
 }
@@ -64,6 +80,11 @@ function hard(){
         box.classList.add('box-hard');
         grid.appendChild(box);
         box.innerHTML= i
+
+        box.addEventListener('click', function(){
+            this.classList.add('bg-azzurro');
+            console.log(i)
+        })
     }
     return box
 }
